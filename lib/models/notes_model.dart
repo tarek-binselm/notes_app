28bdca6643586800e1bfd3b9a390bcaebeb5b503
tarek-binselm <binselmt@gmail.com>
2023-11-01 19:23:30 +0200

    create note model

---
+++ lib/models/notes_model.dart
@@ -0,0 +1,13 @@
+class NotesModel {
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color;
+
+  NotesModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.color,
+  });
+}
